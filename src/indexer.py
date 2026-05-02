f81b3efe237f2669cd9ae76c2e0392461fc3fab6
IvenMilesMaac <sc23imm@leeds.ac.uk>
2026-05-02 07:39:55 +0100

    Implement indexer

---
+++ src/indexer.py
@@ -0,0 +1,16 @@
+def index(pages):
+    index_data = {}
+
+    for url, text in pages.items():
+        words = text.lower().split()
+        for position, word in enumerate(words):
+            if word not in index_data:
+                index_data[word] = {url: {"frequency": 1, "positions": [position]}}
+            else:
+                if url in index_data[word]:
+                    index_data[word][url]["frequency"] += 1
+                    index_data[word][url]["positions"].append(position)
+                else:
+                    index_data[word][url] = {"frequency": 1, "positions": [position]}      
+
+    return index_data
